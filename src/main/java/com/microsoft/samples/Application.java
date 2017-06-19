d954666f35222d3cb24c5a4f8ac89ff905495b2a
Menghua Xiao <menxiao@microsoft.com>
2017-06-19 13:12:38 +0800

    Pre-packaging for docker build

---
+++ src/main/java/com/microsoft/samples/Application.java
@@ -0,0 +1,24 @@
+package com.microsoft.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * An example Spring Boot application.
+ *
+ * @see <a href="https://spring.io/guides/gs/spring-boot-docker/">Spring Boot with Docker</a>
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello, Azure Container Service with Kubernetes";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/com/microsoft/samples/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.microsoft.samples;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit tests for {@link Application}.
+ */
+public class ApplicationTest {
+    @Test
+    public void testHome() {
+        String msg = new Application().home();
+        assertEquals("Hello, Azure Container Service with Kubernetes", msg);
+    }
+}
